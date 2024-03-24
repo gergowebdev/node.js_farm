fe93f03da4563a66845ee0f79398465ffca5b162
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-03-24 16:02:48 +0100

    Filling the templates & parsing variables from URLs

--- index.js
+++ index.js
@@ -3,23 +3,65 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic)
+        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    return output;
+};
+
+const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    "utf-8"
+);
+const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+);
+const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
+);
+
 // API data
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 // Creating the server
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
-    if (pathName === "/" || pathName === "/overview") {
-        res.end("This is the OVERVIEW");
-    } else if (pathName === "/product") {
-        res.end("This is the PRODUCT");
+    // Overview page
+    if (pathname === "/" || pathname === "/overview") {
+        res.writeHead(200, { "Content-type": "text/html" });
+
+        const cardsHTML = dataObj
+            .map((el) => replaceTemplate(tempCard, el))
+            .join("");
+        const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
+        res.end(output);
+
+        // Product Page
+    } else if (pathname === "/product") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
         // Creating the API
-    } else if (pathName === "/api") {
+    } else if (pathname === "/api") {
         res.writeHead(200, { "Content-type": "application/json" });
         res.end(data);
+
+        // Not Found
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
